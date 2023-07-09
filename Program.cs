d0b8e2926e6fd27a962678a3478c4f8493bcba1d
Arseniy <saldeev2008@gmail.com>
2023-07-09 14:36:43 +0500

    FinalFinal

--- Program.cs
+++ Program.cs
@@ -1,199 +1,66 @@
-﻿//HomeWork 1 !!!!
-int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-int range = InputNumbers("Введите диапазон: от 1 до ");
-
-int[,] array = new int[m, n];
-CreateArray(array);
-WriteArray(array);
-
-Console.WriteLine($"\nОтсортированный массив: ");
-OrderArrayLines(array);
-WriteArray(array);
-
-void OrderArrayLines(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)
-      {
-        if (array[i, k] < array[i, k + 1])
-        {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
-        }
-      }
-    }
-  }
-}
-
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
-
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next(range);
-    }
-  }
-}
-
-void WriteArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      Console.Write(array[i, j] + " ");
-    }
-    Console.WriteLine();
-  }
-}
- 
-
-
-
- //HomeWork2 !!!
- Console.Write("Введите размерность m массива: ");
+﻿//Final 1 !!!!
+Console.Write("Введите значение M: ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите размерность n массива: ");
+Console.Write("Введите значение N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int[,] randomArray = new int[m,n];
-
-void mas(int m, int n)
+void che(int m, int n)
 {
-int i,j;
-Random rand = new Random();
-for (i = 0; i < m; i++)
+if (m > n)
+return;
+if (m % 2 == 0)
 {
-for (j = 0; j < n; j++)
+Console.Write($"{m}, ");
+}
+che(m+1,n);
+
+}
+che(m,n);
+
+//Final 2!!!
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa(int m, int n)
 {
-randomArray[i,j] = rand.Next(1,9);
+if (m==n)
+return n;
+else return m + summa(m + 1, n);
 }
-}
-}
+Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {summa(m,n)}");
 
-void printm(int[,] array)
-{
-int i,j;
-for (i = 0; i < array.GetLength(0); i++)
-{
-Console.WriteLine();
-for (j = 0; j < array.GetLength(1); j++)
-{
-Console.Write($"{array[i,j]} ");
-}
-Console.WriteLine();
-}
-}
 
-mas(m,n);
-Console.WriteLine("\nИсходный массив: ");
-printm(randomArray);
 
-int SumLine(int[,] array, int i)
-{
-int sum = array[i,0];
-for (int j = 1; j < array.GetLength(1); j++)
-{
-sum += array[i,j];
-}
-return sum;
-}
-
-int minSum = 1;
-int sum = SumLine(randomArray, 0);
-for (int i = 1; i < randomArray.GetLength(0); i++)
-{
-if (sum > SumLine(randomArray, i))
-{
-sum = SumLine(randomArray, i);
-minSum = i+1;
-}
-}
-Console.WriteLine($"\nСтрока c наименьшей суммой элементов: {minSum}");
-
-//HomeWork 3 !!!
-
-int m = InputNumbers("Введите число строк 1-й матрицы: ");
-int n = InputNumbers("Введите число столбцов 1-й матрицы ");
-int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
-int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
-
-int[,] firstMartrix = new int[m, n];
-CreateArray(firstMartrix);
-Console.WriteLine($"\nПервая матрица:");
-WriteArray(firstMartrix);
-
-int[,] secomdMartrix = new int[n, p];
-CreateArray(secomdMartrix);
-Console.WriteLine($"\nВторая матрица:");
-WriteArray(secomdMartrix);
-
-int[,] resultMatrix = new int[m,p];
-
-MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
-Console.WriteLine($"\nПроизведение первой и второй матриц:");
-WriteArray(resultMatrix);
-
-void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
-{
-  for (int i = 0; i < resultMatrix.GetLength(0); i++)
-  {
-    for (int j = 0; j < resultMatrix.GetLength(1); j++)
-    {
-      int sum = 0;
-      for (int k = 0; k < firstMartrix.GetLength(1); k++)
-      {
-        sum += firstMartrix[i,k] * secomdMartrix[k,j];
-      }
-      resultMatrix[i,j] = sum;
-    }
-  }
-}
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
-
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next(range);
-    }
-  }
-}
-
-void WriteArray (int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      Console.Write(array[i,j] + " ");
-    }
-    Console.WriteLine();
-  }
-}
+//Final 3!!!
 
  
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
 
 
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
 
-
- 
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
